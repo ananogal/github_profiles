5e7801a5c98e2226b0ff5ad350006bb4e125267e
ananogal <ana.nogal@gmail.com>
2014-11-20 15:17:20 +0000

    Added the functionality to detect that a user doesn't exist. update readme file.

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
-#Github Profiles
+Github Profiles
+================
+Search for a user in github and return his profile.
 
-search for a user in github and return his profile. 
+Objective
+==========
+Learn to access APIs with ajax. <br/>
 
+ 
 Tasks :
-1- clear the input box after a search
-2 - add a delete button (or an x) that deletes one entry (article)
-3 - handle the situation where the profile could not be found
+=======
+1- Clear the input box after a search
+2 - Add a delete button (or an x) that deletes one profile
+3 - Handle the situation where the profile could not be found
+
+Things to still to do:
+======================
+Make it pretty :)
---
+++ src/interface.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	$('#fetch_github_profile').on('click', function(){
+		var profile_name = $('#profile_name').val();
+		var source = $('#profileTemplate').html();
+		var template = Handlebars.compile(source);
+		 var url ="https://api.github.com/users/" + profile_name;
+
+		$.get(url + "?client_id=" + keys.client_id +"&client_secret=" + keys.client_secret)
+			.done(function(gitHubProfile){
+				console.log(gitHubProfile);
+				$('#profiles').append(template(gitHubProfile));
+				$('.close_profile').on('click', function(){
+					$(this).closest("article").remove();
+				});
+			}).error(function(){
+				$('.error').html("Couldn't find that user...");
+			});
+		
+		$("#profile_name").val('');
+		$("#profile_name").focus(function(){
+			$('.error').html("");
+		});
+	});
+});
